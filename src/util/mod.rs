8b5c711353175e2e0b7bd27eefde9b71416a085d
bstriker <strikeforcezero@gmail.com>
2024-07-21 01:54:26 -0400

    chore: fix visibility for util modules

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::App;
 
-mod color_id;
-mod color_material_manager;
-mod prototype_mesh_manager;
+pub(crate) mod color_id;
+pub(crate) mod color_material_manager;
+pub(crate) mod prototype_mesh_manager;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((
