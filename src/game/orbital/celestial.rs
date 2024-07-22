6c884316aced71adc7393c3e2b1875e788b7b134
bstriker <strikeforcezero@gmail.com>
2024-07-21 22:02:04 -0400

    snapshot: wip orbital mechanics

--- src/game/orbital/celestial.rs
+++ src/game/orbital/celestial.rs
@@ -1,16 +1,22 @@
 use std::fmt::Formatter;
 
-use avian2d::prelude::{Collider, Mass, RigidBody};
+use avian2d::math::Vector;
+use avian2d::prelude::{Collider, LinearVelocity, Mass, RigidBody};
 use bevy::asset::Assets;
-use bevy::color::palettes::css::RED;
+use bevy::color::palettes::basic::YELLOW;
+use bevy::color::palettes::css::{RED, TEAL};
+use bevy::ecs::query::QueryData;
 use bevy::prelude::{
-    Added, App, Bundle, Circle, Color, Commands, Component, debug, DetectChanges, Entity, Handle,
-    Mesh, Name, Query, Ref, Reflect, ReflectComponent, ResMut, Startup, Update,
+    Added, App, AppGizmoBuilder, Bundle, Circle, Color, Commands, Component, DetectChanges, Entity,
+    FixedUpdate, GizmoConfigGroup, GizmoConfigStore, Gizmos, Handle, Mesh, Mut, Name, Query, Ref,
+    Reflect, ReflectComponent, Res, ResMut, Startup, Transform, Update, Vec2, With,
 };
 use bevy::sprite::{ColorMaterial, ColorMesh2dBundle, Mesh2dHandle};
 use bevy::ui::Display;
-use bevy::utils::default;
+use bevy::utils::{default, EntityHash, EntityHashMap, HashMap};
 use derive_more::Display;
+use itertools::Itertools;
+use log::debug;
 use smart_default::SmartDefault;
 
 use internal_proc_macros::{AutoRegisterType, RegisterTypeBinder};
@@ -21,6 +27,14 @@ use crate::util::prototype_mesh_manager::{PrototypeMesh, PrototypeMeshId, Protot
 pub(crate) fn plugin(app: &mut App) {
     Types.register_types(app);
     app.add_systems(Update, on_added);
+    app.add_systems(FixedUpdate, physics_update);
+    app.init_gizmo_group::<DebugCelestialGizmos>()
+        .add_systems(Update, draw_lines);
+}
+
+#[derive(Default, Reflect, GizmoConfigGroup, AutoRegisterType)]
+pub struct DebugCelestialGizmos {
+    point_map: EntityHashMap<Entity, Vec<Vec2>>,
 }
 
 #[derive(Component, Debug, Copy, Clone, Hash, Eq, PartialEq, Display, SmartDefault)]
@@ -71,6 +85,20 @@ impl CelestialBodyBundle {
             celestial_body_color: CelestialBodyColor(color.into()),
         }
     }
+    pub fn with_transform(mut self, transform: Transform) -> Self {
+        self.mesh.transform = transform;
+        self
+    }
+
+    pub fn with_static(mut self) -> Self {
+        self.rigid_body = RigidBody::Static;
+        self
+    }
+
+    pub fn with_dynamic(mut self) -> Self {
+        self.rigid_body = RigidBody::Dynamic;
+        self
+    }
 }
 
 #[derive(RegisterTypeBinder)]
@@ -108,5 +136,105 @@ fn on_added(
                 prototype_mesh_manager
                     .get_or_create::<CelestialMesh>(&mut meshes, celestial_mesh.into_inner()),
             ));
+    }
+}
+
+#[derive(QueryData)]
+#[query_data(mutable)]
+struct PhysicsUpdateQueryData<'w> {
+    entity: Entity,
+    transform: &'w Transform,
+    mass: &'w Mass,
+    linear_velocity: Mut<'w, LinearVelocity>,
+    collider: &'w Collider,
+}
+
+struct BodyPhysicsData {
+    entity: Entity,
+    mass: f32,
+    pos: Vec2,
+    velocity: Vec2,
+}
+
+impl From<PhysicsUpdateQueryDataReadOnlyItem<'_, '_>> for BodyPhysicsData {
+    fn from(value: PhysicsUpdateQueryDataReadOnlyItem<'_, '_>) -> Self {
+        Self {
+            entity: value.entity,
+            mass: value.mass.0,
+            pos: value.transform.translation.truncate(),
+            velocity: value.linear_velocity.0.into(),
+        }
+    }
+}
+
+fn compute_forces(bodies: &[BodyPhysicsData]) -> Vec<Vec2> {
+    const G: f32 = 6.67430e-11; // gravitational constant in m^3 kg^-1 s^-2
+    let mut forces = vec![Vec2::ZERO; bodies.len()];
+
+    for (i, body_i) in bodies.iter().enumerate() {
+        for (j, body_j) in bodies.iter().enumerate() {
+            if i != j {
+                let r_ij = body_j.pos - body_i.pos;
+                let distance = r_ij.length();
+                if distance > 0.0 {
+                    let force_magnitude = G * body_i.mass * body_j.mass / (distance * distance);
+                    let force_direction = r_ij.normalize();
+                    let force = force_direction * force_magnitude;
+                    forces[i] += force;
+                }
+            }
+        }
+    }
+
+    forces
+}
+
+const FORCE_SCALE: f32 = 10000.0;
+fn physics_update(
+    mut celestial_q: Query<PhysicsUpdateQueryData, With<CelestialBody>>,
+    mut config_store: ResMut<GizmoConfigStore>,
+) {
+    let (_config, debug_gizmos) = config_store.config_mut::<DebugCelestialGizmos>();
+    let bodies = celestial_q
+        .iter()
+        .map(BodyPhysicsData::from)
+        .collect::<Vec<_>>();
+    let forces = compute_forces(&bodies);
+    let force_map = bodies
+        .into_iter()
+        .zip(forces.into_iter())
+        .map(|(b, force)| (b.entity, force))
+        .collect::<HashMap<_, _>>();
+    for mut item in celestial_q.iter_mut() {
+        let Some(force) = force_map.get(&item.entity) else {
+            unreachable!();
+        };
+        let force = *force * FORCE_SCALE;
+        // debug!("{} {force:?}", item.entity);
+        item.linear_velocity.0 += Vector::from(force);
+        let mut entry = debug_gizmos.point_map.entry(item.entity).or_default();
+
+        if let Some(&last) = entry.last() {
+            if last == item.transform.translation.truncate() {
+                continue;
+            }
+        }
+        entry.push(item.transform.translation.truncate());
+    }
+}
+
+fn draw_lines(mut debug_gizmos: Gizmos<DebugCelestialGizmos>) {
+    for (_, points) in debug_gizmos.config_ext.point_map.iter() {
+        let mut color = Color::from(TEAL);
+        for (a, b) in points.iter().rev().tuple_windows() {
+            debug_gizmos.line_2d(*a, *b, color);
+            let mut linear = color.to_linear();
+            linear.alpha -= 0.0001;
+            linear.alpha = linear.alpha.clamp(0.0, 1.0);
+            color = Color::LinearRgba(linear);
+            if linear.alpha == 0.0 {
+                break;
+            }
+        }
     }
 }
