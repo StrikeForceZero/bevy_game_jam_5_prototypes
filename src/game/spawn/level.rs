889228f25b9dbd529a8328c5b27670aeff229c08
bstriker <strikeforcezero@gmail.com>
2024-07-24 04:07:49 -0400

    chore: new celestial body test scene

--- src/game/spawn/level.rs
+++ src/game/spawn/level.rs
@@ -5,6 +5,8 @@ use bevy::color::palettes::basic::{BLUE, GREEN, RED, YELLOW};
 use bevy::color::palettes::css::ORANGE;
 use bevy::prelude::*;
 
+use crate::game::orbital::celestial::CelestialBodyBundle;
+
 pub(super) fn plugin(app: &mut App) {
     app.observe(spawn_level);
 }
@@ -17,43 +19,21 @@ fn spawn_level(_trigger: Trigger<SpawnLevel>, mut commands: Commands) {
     // but add things like walls etc. here.
     // commands.trigger(SpawnPlayer);
 
-    commands.spawn((
-        Name::new("TestBodySun1"),
-        crate::game::orbital::celestial::CelestialBodyBundle::standard(100.0, 500000.0, YELLOW)
-            .with_transform(Transform::from_xyz(0.0, 0.0, 0.0))
-            .with_dynamic(),
-        LinearVelocity::from(Vec2::new(-120.0, 0.0)),
-    ));
+    const MASS_SCALE: f32 = 100000.0; // 2500000000000.0;
 
-    commands.spawn((
-        Name::new("TestBodySun2"),
-        crate::game::orbital::celestial::CelestialBodyBundle::standard(100.0, 500000.0, ORANGE)
-            .with_transform(Transform::from_xyz(0.0, 400.0, 0.0))
-            .with_dynamic(),
-        LinearVelocity::from(Vec2::new(120.0, 0.0)),
-    ));
+    #[rustfmt::skip]
+    let bodies = [
+        ("TestBodySun1", 100.0, 500000.0, YELLOW, Vec2::new(0.0, 0.0), Vec2::new(-15.0, -5.0)),
+        ("TestBodySun2", 100.0, 500000.0, ORANGE, Vec2::new(0.0, 400.0), Vec2::new(15.0, 5.0)),
+        ("TestBodyA", 15.0, 1500.0, RED, Vec2::new(-400.0, 0.0), Vec2::new(20.0, 15.0)),
+        ("TestBodyB", 50.0, 10000.0, BLUE, Vec2::new(400.0, 0.0), Vec2::new(-10.0, -20.0)),
+        ("TestBodyB", 30.0, 1000.0, GREEN, Vec2::new(400.0, 400.0), Vec2::new(5.0, -35.0)),
+    ];
 
-    commands.spawn((
-        Name::new("TestBodyA"),
-        crate::game::orbital::celestial::CelestialBodyBundle::standard(15.0, 1500.0, RED)
-            .with_transform(Transform::from_xyz(-400.0, 0.0, 0.0))
-            .with_dynamic(),
-        LinearVelocity::from(Vec2::new(10.0, 30.0)),
-    ));
-
-    commands.spawn((
-        Name::new("TestBodyB"),
-        crate::game::orbital::celestial::CelestialBodyBundle::standard(50.0, 10000.0, BLUE)
-            .with_transform(Transform::from_xyz(400.0, 0.0, 0.0))
-            .with_dynamic(),
-        LinearVelocity::from(Vec2::new(-50.0, -50.0)),
-    ));
-
-    commands.spawn((
-        Name::new("TestBodyB"),
-        crate::game::orbital::celestial::CelestialBodyBundle::standard(30.0, 1000.0, GREEN)
-            .with_transform(Transform::from_xyz(400.0, 400.0, 0.0))
-            .with_dynamic(),
-        LinearVelocity::from(Vec2::new(10.0, -50.0)),
-    ));
+    for (name, radius, mass, color, pos, vel) in bodies {
+        let body = CelestialBodyBundle::standard(radius, mass * MASS_SCALE, color)
+            .with_transform(Transform::from_translation(pos.extend(0.0)))
+            .with_dynamic();
+        commands.spawn((Name::new(name), body, LinearVelocity::from(vel)));
+    }
 }
